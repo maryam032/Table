39774271ea833f0fce3954d725817675921ff555
maryam032 <maryamakram032@gmail.com>
2019-08-01 05:19:59 +0500

    Delete row button added

--- src/Table.js
+++ src/Table.js
@@ -2,33 +2,50 @@ import React from 'react'
 import './Table.css'
 
 
+
 const dataArray = [
-    {enroll:'013',name:'Maryam',contact:'0307-3781740',address:'F8/3,Islamabad',semester:'5th'},
-    {enroll:'028',name:'Talha',contact:'0307-3781740',address:'F8/3,Islamabad',semester:'5th'}
+    {Enroll:'013',Name:'Maryam',Contact:'0307-3781740',Address:'F8/3,Islamabad',Semester:'5th'},
+    {Enroll:'028',Name:'Talha',Contact:'0307-3781740',Address:'F8/3,Islamabad',Semester:'5th'}
 ]
 
 class Table extends React.Component{
     constructor(props){
-
         super(props);
         this.state=({
             data:dataArray,
-            enroll:'',
-            name:'',
-            contact:'',
-            address:'',
-            semester:''
+            Enroll:'',
+            Name:'',
+            Contact:'',
+            Address:'',
+            Semester:''
         })
     }
     AddRecord=()=>{
-        const addTemp = {enroll:this.state.enroll,
-        name:this.state.name,
-        contact:this.state.contact,
-        address:this.state.address,
-        semester:this.state.semester}
+        const addTemp = {Enroll:this.state.Enroll,
+        Name:this.state.Name,
+        Contact:this.state.Contact,
+        Address:this.state.Address,
+        Semester:this.state.Semester}
         dataArray.push(addTemp)
         console.log(dataArray)
         this.setState({data:dataArray})
+        
+        
+        this.setState({
+            Enroll:'',
+            Name:'',
+            Contact:'',
+            Address:'',
+            Semester:''
+
+        })
+    }
+    handleDelete=(index)=>
+    {
+        dataArray.splice(index,1)
+        this.setState({
+            data:dataArray
+        })
     }
     onChangeHandler=(e)=>
     {
@@ -38,33 +55,42 @@ class Table extends React.Component{
     }
     render()
     {
+        
+        
         return(
-            <div  style={{padding:50,border:'1px solid black',height:700,width:'100%',backgroundColor:'purple'}}>
-                <input type="button" value="Add Record" onClick={this.AddRecord}/>
-                <input type="text"  name="enroll" value={this.state.enroll} placeholder="enrolllment" onChange={e=>this.onChangeHandler(e)}/>
-                <input type="text" name="name" value={this.state.name} placeholder="name" onChange={e=>this.onChangeHandler(e)}/>
-                <input type="text" name="contact" value={this.state.contact} placeholder="contact" onChange={e=>this.onChangeHandler(e)}/>
-                <input type="text" name="address" value={this.state.address} placeholder="address" onChange={e=>this.onChangeHandler(e)}/>
-                <input type="text" name="semester" value={this.state.semester} placeholder="semester" onChange={e=>this.onChangeHandler(e)}/>
-
+            <div  style={{marginLeft:60,height:600,width:'90%',backgroundColor:'#FFFCE5',borderRadius:30,marginTop:0}}>
+                <h1 style={{textAlign:'center'}}>Student Record</h1>
+                <div style={{borderColor:'#008080', marginLeft:100,display:'block',marginTop:60 ,width:1100}}>
+                <input style={{fontSize:20,width:200,height:45,outline:'none',border:'1px solid #D3D3D3',paddingLeft:5}} type="text"  name="Enroll" value={this.state.Enroll} placeholder="Enrolllment" onChange={e=>this.onChangeHandler(e)} />
+                <input style={{ fontSize:20,width:200,height:45,outline:'none',border:'1px solid #D3D3D3',paddingLeft:5,marginLeft:10}}type="text" name="Name" value={this.state.Name} placeholder="Name" onChange={e=>this.onChangeHandler(e)}/>
+                <input style={{ fontSize:20,width:200,height:45,outline:'none',border:'1px solid #D3D3D3',paddingLeft:5,marginLeft:10}} type="text" name="Contact" value={this.state.Contact} placeholder="Contact" onChange={e=>this.onChangeHandler(e)}/>
+                <input style={{ fontSize:20,width:200,height:45,outline:'none',border:'1px solid #D3D3D3',paddingLeft:5,marginLeft:10}} type="text" name="Address" value={this.state.Address} placeholder="Address" onChange={e=>this.onChangeHandler(e)}/>
+                <input style={{ fontSize:20,width:160,height:45,outline:'none',border:'1px solid #D3D3D3',paddingLeft:5,marginLeft:10}}type="text" name="Semester" value={this.state.Semester} placeholder="Semester" onChange={e=>this.onChangeHandler(e)}/>
+                </div>
+                <input  style={{marginLeft:960,display:'block' ,marginTop:20,borderRadius:'3px',border:'none',outline:'none',width:170,height:50,backgroundColor:'#2E8B57',fontSize:20,color:'white'}} className="Button" type="button" value="Add Record" onClick={this.AddRecord}/>
                <table className="Table">
-                    <tr>
+                    <tr >
                         <th>Enrollment</th>
                         <th>Name</th>
                         <th>Contact</th>
                         <th>Address</th>
                         <th>Semester</th>
+                        
                     </tr>
                     
                     {
                         this.state.data.map((obj,index)=>{
                             return(
                                 <tr>
-                                    <td>{obj.enroll}</td>
-                                    <td>{obj.name}</td>
-                                    <td>{obj.contact}</td>
-                                    <td>{obj.address}</td>
-                                    <td>{obj.semester}</td>
+                                    <td>{obj.Enroll}</td>
+                                    <td>{obj.Name}</td>
+                                    <td>{obj.Contact}</td>
+                                    <td>{obj.Address}</td>
+                                    <td style={{float:'left',width:'192px',height:'40px',paddingTop:20}}>{obj.Semester}
+                                    <img onClick={e=>this.handleDelete(index)}style={{outline:'none',display:'block',width:"40px" ,float:'right',marginTop:0,marginRight:4,marginBottom:15}}src={process.env.PUBLIC_URL +"/del (2).png"}
+                                    alt="Mypic" /> </td>
+                                    
+                
                                 </tr>
                             )
                     })
